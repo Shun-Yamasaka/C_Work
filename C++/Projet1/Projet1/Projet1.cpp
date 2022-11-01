bf60026fcd701d7f5e4f058061a091d4e6976141
shun yamasaka <118syuunico@gmail.com>
2022-11-01 21:32:43 +0900

    C++のサンプルプロジェクト追加(Hello World)

---
+++ C++/Projet1/Projet1/Projet1.cpp
@@ -0,0 +1,21 @@
+// Projet1.cpp : メイン プロジェクト ファイルです。
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+using namespace System;
+using std::cout; using std::cin;
+using std::endl; using std::vector;
+using std::copy;
+
+//int main(array<System::String ^> ^args)
+//{
+//    Console::WriteLine(L"Hello World");
+//    return 0;
+//}
+int main()
+{
+	std::cout << "Hello, world" << std ::endl;
+	cin.get();
+}

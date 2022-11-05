4e5511f4cc34254ad7a6b4c4b6b3d86dd640c8a3
shun yamasaka <118syuunico@gmail.com>
2022-11-05 12:56:08 +0900

    BMIを求めるプログラム 肥満かどうかチェックする処理を含む

---
+++ C++/スラスラC++/list3_1.cpp
@@ -0,0 +1,52 @@
+/* BMIを求めるプログラム */
+/* if～else文バージョン */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    /*------------------*/
+    /* 定数定義/変数宣言 */
+    /*------------------*/
+    const int STD_BMI = 22;  // 標準BMI
+    double height;           // 身長
+    double weight;           // 体重
+    double bmi;              // BMI
+    double stdWeight;        // 標準体重
+
+    /*----------*/
+    /* 数値入力 */
+    /*----------*/
+    // キー入力をheightに格納する（身長）
+    cout << "input height(cm):";
+    cin >> height;
+    height /= 100; // cm単位からm単位に変換
+    // キー入力をweightに格納する（体重）
+    cout << "input weight(kg):";
+    cin >> weight;
+
+    /*----------*/
+    /* 演算処理 */
+    /*----------*/
+    // 身長と体重からBMIを計算する
+    bmi = weight / height / height;
+    // 標準体重を計算する
+    stdWeight = STD_BMI * height * height;
+
+    /*----------*/
+    /* 結果出力 */
+    /*----------*/
+    // BMIを画面に表示する（小数点以下1桁で表示）
+    cout << "BMI=" << fixed << setprecision(1) << bmi << endl;
+    // 標準体重を画面に表示する
+    cout << "STD_Weight=" << stdWeight << endl;
+
+    // 肥満かどうかを判定した結果を表示する
+    if (bmi >= 25.0){
+        cout << "肥満です" << endl;
+    } else{
+        cout << "肥満ではありません" << endl;
+    }
+
+    return 0;
+}

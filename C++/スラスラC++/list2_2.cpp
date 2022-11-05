73b2c9328eedb47029ddd61de6992475bc190586
shun yamasaka <118syuunico@gmail.com>
2022-11-05 11:42:06 +0900

    ネームスペースの役割確認

---
+++ C++/スラスラC++/list2_2.cpp
@@ -0,0 +1,8 @@
+/* using namespace std;の役割を確認するプログラム */
+/* コンパイルエラーとなる */
+#include <iostream>
+
+int main() {
+    cout << "hello, world" << endl;
+    return 0;
+}
---
+++ C++/スラスラC++/list2_3.cpp
@@ -0,0 +1,7 @@
+/* coutとendlをstd::coutとstd::endlに書き換えたプログラム */
+#include <iostream>
+
+int main(){
+    std::cout<< "hello, world" << std::endl;
+    return 0;
+}
